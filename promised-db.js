9ca34269b25e5cafae54e95cf186b38d697c0c56
Arthur Langereis <puurtuur@me.com>
2017-03-13 13:15:10 +0100

    new build products

--- promised-db.js
+++ promised-db.js
@@ -1,179 +1,163 @@
 // promised-db - IndexedDB wrapped in a promise-based API with contextual methods and timeout support. (https://github.com/zenmumbler/promised-db)
 // (c) 2016-Present by Arthur Langereis (@zenmumbler)
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
+export class PromisedDB {
+    constructor(name, version, upgrade) {
+        this.db_ = this._request(indexedDB.open(name, version), openReq => {
+            openReq.onupgradeneeded = upgradeEvt => {
+                const db = openReq.result;
+                upgrade(db, upgradeEvt.oldVersion, upgradeEvt.newVersion || version);
+            };
+        })
+            .catch(error => {
+            console.warn(`PromisedDB: failed to open / upgrade database '${name}'`, error);
+        });
+        // the TransactionContext is implemented as the private methods in PDB
+        // bound to this and exposed as loose functions.
+        this.tctx_ = {
+            request: this._request.bind(this),
+            cursor: this._cursor.bind(this),
+            keyCursor: this._keyCursor.bind(this),
+            getAll: this._getAll.bind(this),
+            getAllKeys: this._getAllKeys.bind(this)
+        };
     }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
+    close() {
+        this.db_.then(db => {
+            db.close();
+        });
     }
-})(function (require, exports) {
-    "use strict";
-    var PromisedDB = (function () {
-        function PromisedDB(name, version, upgrade) {
-            this.db_ = this._request(indexedDB.open(name, version), function (openReq) {
-                openReq.onupgradeneeded = function (upgradeEvt) {
-                    var db = openReq.result;
-                    upgrade(db, upgradeEvt.oldVersion, upgradeEvt.newVersion || version);
+    transaction(storeNames, mode, fn) {
+        return this.db_.then(db => {
+            return new Promise((resolve, reject) => {
+                const tr = db.transaction(storeNames, mode);
+                tr.onerror = () => {
+                    cancelTimeout();
+                    reject(tr.error || "transaction failed");
                 };
-            })
-                .catch(function (error) {
-                console.warn("PromisedDB: failed to open / upgrade database '" + name + "'", error);
-            });
-            // the TransactionContext is implemented as the private methods in PDB
-            // bound to this and exposed as loose functions.
-            this.tctx_ = {
-                request: this._request.bind(this),
-                cursor: this._cursor.bind(this),
-                keyCursor: this._keyCursor.bind(this),
-                getAll: this._getAll.bind(this),
-                getAllKeys: this._getAllKeys.bind(this)
-            };
-        }
-        PromisedDB.prototype.close = function () {
-            this.db_.then(function (db) {
-                db.close();
-            });
-        };
-        PromisedDB.prototype.transaction = function (storeNames, mode, fn) {
-            var _this = this;
-            return this.db_.then(function (db) {
-                return new Promise(function (resolve, reject) {
-                    var tr = db.transaction(storeNames, mode);
-                    tr.onerror = function () {
-                        cancelTimeout();
-                        reject(tr.error || "transaction failed");
-                    };
-                    tr.onabort = function () {
-                        cancelTimeout();
-                        reject("aborted");
-                    };
-                    var timeoutID = null;
-                    var cancelTimeout = function () {
-                        if (timeoutID !== null) {
-                            clearTimeout(timeoutID); // make timeouts work for both web and node contexts
-                            timeoutID = null;
-                        }
-                    };
-                    var tc = Object.create(_this.tctx_, {
-                        timeout: {
-                            value: function (ms) {
-                                timeoutID = setTimeout(function () {
-                                    timeoutID = null;
-                                    tr.abort();
-                                }, ms);
-                            }
-                        }
-                    });
-                    var result = fn(tr, tc);
-                    tr.oncomplete = function () {
-                        cancelTimeout();
-                        resolve((result === undefined) ? undefined : result);
-                    };
-                });
-            });
-        };
-        PromisedDB.prototype._request = function (req, fn) {
-            var reqProm = new Promise(function (resolve, reject) {
-                req.onerror = function () { reject(req.error || "request failed"); };
-                req.onsuccess = function () { resolve(req.result); };
-                if (fn) {
-                    fn(req);
-                }
-            });
-            return this.db_ ? this.db_.then(function () { return reqProm; }) : reqProm;
-        };
-        PromisedDB.prototype._cursorImpl = function (cursorReq) {
-            var result = {
-                next: function (callback) {
-                    this.callbackFn_ = callback;
-                    return this;
-                },
-                complete: function (callback) {
-                    this.completeFn_ = callback;
-                    return this;
-                },
-                catch: function (callback) {
-                    this.errorFn_ = callback;
-                    return this;
-                }
-            };
-            cursorReq.onerror = function () {
-                if (result.errorFn_) {
-                    result.errorFn_(cursorReq.error);
-                }
-            };
-            cursorReq.onsuccess = function () {
-                var cursor = cursorReq.result;
-                if (cursor) {
-                    if (result.callbackFn_) {
-                        result.callbackFn_(cursor);
+                tr.onabort = () => {
+                    cancelTimeout();
+                    reject("aborted");
+                };
+                let timeoutID = null;
+                const cancelTimeout = function () {
+                    if (timeoutID !== null) {
+                        clearTimeout(timeoutID); // make timeouts work for both web and node contexts
+                        timeoutID = null;
                     }
+                };
+                const tc = Object.create(this.tctx_, {
+                    timeout: {
+                        value: function (ms) {
+                            timeoutID = setTimeout(function () {
+                                timeoutID = null;
+                                tr.abort();
+                            }, ms);
+                        }
+                    }
+                });
+                const result = fn(tr, tc);
+                tr.oncomplete = () => {
+                    cancelTimeout();
+                    resolve(result);
+                };
+            });
+        });
+    }
+    _request(req, fn) {
+        const reqProm = new Promise(function (resolve, reject) {
+            req.onerror = () => { reject(req.error || "request failed"); };
+            req.onsuccess = () => { resolve(req.result); };
+            if (fn) {
+                fn(req);
+            }
+        });
+        return this.db_ ? this.db_.then(() => reqProm) : reqProm;
+    }
+    _cursorImpl(cursorReq) {
+        const result = {
+            next: function (callback) {
+                this.callbackFn_ = callback;
+                return this;
+            },
+            complete: function (callback) {
+                this.completeFn_ = callback;
+                return this;
+            },
+            catch: function (callback) {
+                this.errorFn_ = callback;
+                return this;
+            }
+        };
+        cursorReq.onerror = function () {
+            if (result.errorFn_) {
+                result.errorFn_(cursorReq.error);
+            }
+        };
+        cursorReq.onsuccess = function () {
+            const cursor = cursorReq.result;
+            if (cursor) {
+                if (result.callbackFn_) {
+                    result.callbackFn_(cursor);
+                }
+            }
+            else {
+                if (result.completeFn_) {
+                    result.completeFn_();
+                }
+            }
+        };
+        return result;
+    }
+    _cursor(container, range, direction) {
+        const cursorReq = container.openCursor(range, direction);
+        return this._cursorImpl(cursorReq);
+    }
+    // IDB 2 has IDBObjectStore.openKeyCursor, but 1 does not
+    _keyCursor(index, range, direction) {
+        const cursorReq = index.openKeyCursor(range, direction);
+        return this._cursorImpl(cursorReq);
+    }
+    _getAll(container, range, direction, limit) {
+        return new Promise((resolve, reject) => {
+            const result = [];
+            this._cursor(container, range, direction)
+                .next(cur => {
+                result.push(cur.value);
+                if (limit && (result.length === limit)) {
+                    resolve(result);
                 }
                 else {
-                    if (result.completeFn_) {
-                        result.completeFn_();
-                    }
+                    cur.continue();
                 }
-            };
-            return result;
-        };
-        PromisedDB.prototype._cursor = function (container, range, direction) {
-            var cursorReq = container.openCursor(range, direction);
-            return this._cursorImpl(cursorReq);
-        };
-        // IDB 2 has IDBObjectStore.openKeyCursor, but 1 does not
-        PromisedDB.prototype._keyCursor = function (index, range, direction) {
-            var cursorReq = index.openKeyCursor(range, direction);
-            return this._cursorImpl(cursorReq);
-        };
-        PromisedDB.prototype._getAll = function (container, range, direction, limit) {
-            var _this = this;
-            return new Promise(function (resolve, reject) {
-                var result = [];
-                _this._cursor(container, range, direction)
-                    .next(function (cur) {
-                    result.push(cur.value);
-                    if (limit && (result.length === limit)) {
-                        resolve(result);
-                    }
-                    else {
-                        cur.continue();
-                    }
-                })
-                    .complete(function () {
-                    resolve(result);
-                })
-                    .catch(function (error) {
-                    reject(error);
-                });
+            })
+                .complete(() => {
+                resolve(result);
+            })
+                .catch(error => {
+                reject(error);
             });
-        };
-        PromisedDB.prototype._getAllKeys = function (container, range, direction, limit) {
-            var _this = this;
-            return new Promise(function (resolve, reject) {
-                var result = [];
-                _this._keyCursor(container, range, direction)
-                    .next(function (cur) {
-                    result.push(cur.primaryKey);
-                    if (limit && (result.length === limit)) {
-                        resolve(result);
-                    }
-                    else {
-                        cur.continue();
-                    }
-                })
-                    .complete(function () {
+        });
+    }
+    _getAllKeys(container, range, direction, limit) {
+        return new Promise((resolve, reject) => {
+            const result = [];
+            this._keyCursor(container, range, direction)
+                .next(cur => {
+                result.push(cur.primaryKey);
+                if (limit && (result.length === limit)) {
                     resolve(result);
-                })
-                    .catch(function (error) {
-                    reject(error);
-                });
+                }
+                else {
+                    cur.continue();
+                }
+            })
+                .complete(() => {
+                resolve(result);
+            })
+                .catch(error => {
+                reject(error);
             });
-        };
-        return PromisedDB;
-    }());
-    exports.PromisedDB = PromisedDB;
-});
+        });
+    }
+}
 //# sourceMappingURL=promised-db.js.map
