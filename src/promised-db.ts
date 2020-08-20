dbd3e1b66be761b3cf2194aa8a022061b1130e70
Arthur Langereis <puurtuur@me.com>
2020-08-20 18:54:20 +0200

    ref: complete refactor and modernisation of code

--- src/promised-db.ts
+++ src/promised-db.ts
@@ -1,214 +1,160 @@
 // promised-db - IndexedDB wrapped in a promise-based API with contextual methods and timeout support. (https://github.com/zenmumbler/promised-db)
-// (c) 2016-Present by Arthur Langereis (@zenmumbler)
+// (c) 2016-Present by @zenmumbler
 
-export type PDBUpgradeCallback = (db: IDBDatabase, fromVersion: number, toVersion: number) => void;
+export type PDBTransactionCallback<T> = (tr: IDBTransaction, context: PDBTransactionContext) => Promise<T> | T;
+export type PDBUpgradeCallback = (db: IDBDatabase, tr: IDBTransaction, fromVersion: number, toVersion: number) => void;
 
 export type PDBTransactionMode = "readonly" | "readwrite";
-export type PDBTransactionRequestFn = (req: IDBRequest, fn?: (req: IDBRequest) => void) => Promise<any>;
-export interface PDBTransactionContextBase {
-	readonly request: PDBTransactionRequestFn;
-	readonly cursor: (container: IDBIndex | IDBObjectStore, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection) => PDBCursorResult<IDBCursorWithValue>;
-	readonly keyCursor: (index: IDBIndex, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection) => PDBCursorResult<IDBCursor>;
-	readonly getAll: <T>(container: IDBIndex | IDBObjectStore, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection, limit?: number) => Promise<T[]>;
-	readonly getAllKeys: (index: IDBIndex, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection, limit?: number) => Promise<IDBValidKey[]>;
-}
-export interface PDBTransactionContext extends PDBTransactionContextBase {
-	readonly timeout: (ms: number) => void;
+export interface PDBTransactionContext {
+	request: <T>(req: IDBRequest, fn?: (req: IDBRequest) => void) => Promise<T>;
+	cursor: (container: IDBIndex | IDBObjectStore, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection) => PDBCursor<IDBCursorWithValue>;
+	keyCursor: (index: IDBIndex, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection) => PDBCursor<IDBCursor>;
+	timeout: (ms: number) => void;
 }
 
 export type PDBCursorDirection = "next" | "prev" | "nextunique" | "prevunique";
 
-export interface PDBCursorResult<C extends IDBCursor> {
-	next(callback: (cursor: C) => void): PDBCursorResult<C>;
-	complete(callback: () => void): PDBCursorResult<C>;
-	catch(callback: (error: any) => void): PDBCursorResult<C>;
+export interface PDBCursor<C extends IDBCursor> {
+	next(callback: (cursor: C) => void): PDBCursor<C>;
+	complete(callback: () => void): PDBCursor<C>;
+	catch(callback: (error: any) => void): PDBCursor<C>;
 }
-interface PDBCursorBuilder<C extends IDBCursor> extends PDBCursorResult<C> {
+interface PDBCursorBuilder<C extends IDBCursor> extends PDBCursor<C> {
 	callbackFn_?: (cursor: C) => void;
 	completeFn_?: () => void;
 	errorFn_?: (error: any) => void;
 }
 
+export interface PromisedDB {
+	transaction<T>(storeNames: string | string[], mode: PDBTransactionMode, fn: (tr: IDBTransaction, context: PDBTransactionContext) => Promise<T> | T): Promise<T>;
+	close(): void;	
+}
 
-export class PromisedDB {
-	private db_: Promise<IDBDatabase>;
-	private tctx_: PDBTransactionContextBase;
-
-	constructor(name: string, version: number, upgrade: PDBUpgradeCallback) {
-		this.db_ = this._request(indexedDB.open(name, version),
-			openReq => {
-				openReq.onupgradeneeded = upgradeEvt => {
-					const db = openReq.result as IDBDatabase;
-					upgrade(db, upgradeEvt.oldVersion, upgradeEvt.newVersion || version);
-				};
-			})
-			.catch(error => {
-				console.warn(`PromisedDB: failed to open / upgrade database '${name}'`, error);
-			});
-
-		// the TransactionContext is implemented as the private methods in PDB
-		// bound to this and exposed as loose functions.
-		this.tctx_ = {
-			request: this._request.bind(this),
-			cursor: this._cursor.bind(this),
-			keyCursor: this._keyCursor.bind(this),
-			getAll: this._getAll.bind(this),
-			getAllKeys: this._getAllKeys.bind(this)
+export function openDatabase(name: string, version: number, upgrade: PDBUpgradeCallback) {
+	return new Promise<PromisedDB>(function(resolve, reject) {
+		const req = indexedDB.open(name, version);
+		req.onerror = () => { reject(req.error || `Could not open database "${name}"`); };
+		req.onblocked = () => { reject("Database is outdated but cannot be upgraded because it is still being used elsewhere."); };
+		req.onupgradeneeded = upgradeEvt => {
+			const db = req.result;
+			db.onerror = () => { reject("An error occurred while updating the database"); };
+			upgrade(db, req.transaction!, upgradeEvt.oldVersion, upgradeEvt.newVersion || version);
 		};
+		req.onsuccess = () => {
+			const db = req.result;
+			db.onerror = null;
+			resolve(new PromisedDBImpl(db));
+		};
+	});
+}
+
+function request<R extends IDBRequest>(req: R, fn?: (req: R) => void): Promise<any> {
+	return new Promise<any>((resolve, reject) => {
+		req.onerror = () => { reject(req.error || "request failed"); };
+		req.onsuccess = () => { resolve(req.result); };
+
+		if (fn) {
+			fn(req);
+		}
+	});
+}
+
+function cursorImpl<C extends IDBCursor>(cursorReq: IDBRequest): PDBCursor<C> {
+	const result: PDBCursorBuilder<C> = {
+		next: function(this: PDBCursorBuilder<C>, callback: (cursor: C) => void): PDBCursor<C> {
+			this.callbackFn_ = callback;
+			return this;
+		},
+		complete: function(this: PDBCursorBuilder<C>, callback: () => void): PDBCursor<C> {
+			this.completeFn_ = callback;
+			return this;
+		},
+		catch: function(this: PDBCursorBuilder<C>, callback: (error: any) => void): PDBCursor<C> {
+			this.errorFn_ = callback;
+			return this;
+		}
+	};
+
+	cursorReq.onerror = function() {
+		if (result.errorFn_) {
+			result.errorFn_(cursorReq.error);
+		}
+	};
+	cursorReq.onsuccess = function() {
+		const cursor = cursorReq.result as C | undefined;
+		if (cursor) {
+			if (result.callbackFn_) {
+				result.callbackFn_(cursor);
+			}
+		}
+		else {
+			if (result.completeFn_) {
+				result.completeFn_();
+			}
+		}
+	};
+
+	return result;
+}
+
+function cursor(container: IDBIndex | IDBObjectStore, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection) {
+	const cursorReq = container.openCursor(range, direction);
+	return cursorImpl<IDBCursorWithValue>(cursorReq);
+}
+
+function keyCursor(container: IDBIndex | IDBObjectStore, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection) {
+	const cursorReq = container.openKeyCursor(range, direction);
+	return cursorImpl(cursorReq);
+}
+
+class PromisedDBImpl {
+	private db_: IDBDatabase;
+
+	constructor(db: IDBDatabase) {
+		this.db_ = db;
 	}
 
 	close() {
-		this.db_.then(db => {
-			db.close();
-		});
+		this.db_.close();
 	}
 
 	transaction<T>(storeNames: string | string[], mode: PDBTransactionMode, fn: (tr: IDBTransaction, context: PDBTransactionContext) => Promise<T> | T): Promise<T> {
-		return this.db_.then(db => {
-			return new Promise<T>((resolve, reject) => {
-				const tr = db.transaction(storeNames, mode);
-				tr.onerror = () => {
-					cancelTimeout();
-					reject(tr.error || "transaction failed");
-				};
-				tr.onabort = () => {
-					cancelTimeout();
-					reject("aborted");
-				};
+		return new Promise<T>((resolve, reject) => {
+			let timeoutID: number | undefined;
+			const cancelTimeout = function() {
+				if (timeoutID !== undefined) {
+					clearTimeout(timeoutID);
+					timeoutID = undefined;
+				}
+			};
 
-				let timeoutID: number | undefined = undefined;
-				const cancelTimeout = function() {
-					if (timeoutID !== undefined) {
-						clearTimeout(timeoutID);
+			const tr = this.db_.transaction(storeNames, mode);
+			tr.onerror = () => {
+				cancelTimeout();
+				reject(tr.error || "transaction failed");
+			};
+			tr.onabort = () => {
+				cancelTimeout();
+				reject("aborted");
+			};
+			tr.oncomplete = () => {
+				cancelTimeout();
+				resolve(result);
+			};
+
+			const tc: PDBTransactionContext = {
+				request,
+				cursor,
+				keyCursor,
+				timeout(ms: number) {
+					timeoutID = setTimeout(function() {
 						timeoutID = undefined;
-					}
-				};
-
-				const tc: PDBTransactionContext = Object.create(this.tctx_, {
-					timeout: {
-						value(ms: number) {
-							timeoutID = setTimeout(function() {
-								timeoutID = undefined;
-								tr.abort();
-							}, ms);
-						}
-					}
-				});
-
-				const result = fn(tr, tc);
-				tr.oncomplete = () => {
-					cancelTimeout();
-					resolve(result);
-				};
-			});
-		});
-	}
-
-	private _request<R extends IDBRequest>(req: R, fn?: (req: R) => void): Promise<any> {
-		const reqProm = new Promise<any>(function(resolve, reject) {
-				req.onerror = () => { reject(req.error || "request failed"); };
-				req.onsuccess = () => { resolve(req.result); };
-
-				if (fn) {
-					fn(req);
+						tr.abort();
+					}, ms);
 				}
-			});
+			};
 
-		return this.db_ ? this.db_.then(() => reqProm) : reqProm;
-	}
-
-	private _cursorImpl<C extends IDBCursor>(cursorReq: IDBRequest): PDBCursorResult<C> {
-		const result: PDBCursorBuilder<C> = {
-			next: function(this: PDBCursorBuilder<C>, callback: (cursor: C) => void): PDBCursorResult<C> {
-				this.callbackFn_ = callback;
-				return this;
-			},
-			complete: function(this: PDBCursorBuilder<C>, callback: () => void): PDBCursorResult<C> {
-				this.completeFn_ = callback;
-				return this;
-			},
-			catch: function(this: PDBCursorBuilder<C>, callback: (error: any) => void): PDBCursorResult<C> {
-				this.errorFn_ = callback;
-				return this;
-			}
-		};
-
-		cursorReq.onerror = function() {
-			if (result.errorFn_) {
-				result.errorFn_(cursorReq.error);
-			}
-		};
-		cursorReq.onsuccess = function() {
-			const cursor = cursorReq.result as C | undefined;
-			if (cursor) {
-				if (result.callbackFn_) {
-					result.callbackFn_(cursor);
-				}
-			}
-			else {
-				if (result.completeFn_) {
-					result.completeFn_();
-				}
-			}
-		};
-
-		return result;
-	}
-
-	private _cursor(container: IDBIndex | IDBObjectStore, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection) {
-		const cursorReq = container.openCursor(range, direction);
-		return this._cursorImpl<IDBCursorWithValue>(cursorReq);
-	}
-
-	// IDB 2 has IDBObjectStore.openKeyCursor, but 1 does not
-	private _keyCursor(index: IDBIndex, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection) {
-		const cursorReq = index.openKeyCursor(range, direction);
-		return this._cursorImpl(cursorReq);
-	}
-
-	private _getAll<T>(container: IDBIndex | IDBObjectStore, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection, limit?: number) {
-		return new Promise<T[]>((resolve, reject) => {
-			const result: T[] = [];
-
-			this._cursor(container, range, direction)
-				.next(cur => {
-					result.push(cur.value as T);
-					if (limit && (result.length === limit)) {
-						resolve(result);
-					}
-					else {
-						cur.continue();
-					}
-				})
-				.complete(() => {
-					resolve(result);
-				})
-				.catch(error => {
-					reject(error);
-				});
-		});
-	}
-
-	private _getAllKeys(container: IDBIndex, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection, limit?: number) {
-		return new Promise<IDBValidKey[]>((resolve, reject) => {
-			const result: IDBValidKey[] = [];
-
-			this._keyCursor(container, range, direction)
-				.next(cur => {
-					result.push(cur.primaryKey);
-					if (limit && (result.length === limit)) {
-						resolve(result);
-					}
-					else {
-						cur.continue();
-					}
-				})
-				.complete(() => {
-					resolve(result);
-				})
-				.catch(error => {
-					reject(error);
-				});
+			const result = fn(tr, tc);
 		});
 	}
 }
