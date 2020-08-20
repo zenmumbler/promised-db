2c843685632c4d7ab6eb254beeaa1f3515e7bf7a
Arthur Langereis <puurtuur@me.com>
2020-08-20 18:53:44 +0200

    chore: update file layout

---
+++ dist/promised-db.d.ts
@@ -0,0 +1,30 @@
+// promised-db - IndexedDB wrapped in a promise-based API with contextual methods and timeout support.
+// (c) 2016-Present by @zenmumbler
+
+declare module "promised-db" {
+	type PDBTransactionMode = "readonly" | "readwrite";
+	type PDBCursorDirection = "next" | "prev" | "nextunique" | "prevunique";
+
+	interface PDBCursor<C extends IDBCursor> {
+		next(callback: (cursor: C) => void): PDBCursor<C>;
+		complete(callback: () => void): PDBCursor<C>;
+		catch(callback: (error: any) => void): PDBCursor<C>;
+	}
+
+	interface PDBTransactionContext {
+		request: <T>(req: IDBRequest, fn?: (req: IDBRequest) => void) => Promise<T>;
+		cursor: (container: IDBIndex | IDBObjectStore, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection) => PDBCursor<IDBCursorWithValue>;
+		keyCursor: (index: IDBIndex, range?: IDBKeyRange | IDBValidKey, direction?: PDBCursorDirection) => PDBCursor<IDBCursor>;
+		timeout: (ms: number) => void;
+	}
+	
+	type PDBTransactionCallback<T> = (tr: IDBTransaction, context: PDBTransactionContext) => Promise<T> | T;
+	type PDBUpgradeCallback = (db: IDBDatabase, tr: IDBTransaction, fromVersion: number, toVersion: number) => void;
+	
+	interface PromisedDB {
+		transaction<T>(storeNames: string | string[], mode: PDBTransactionMode, fn: PDBTransactionCallback<T>): Promise<T>;
+		close(): void;
+	}
+
+	function openDatabase(name: string, version: number, upgrade: PDBUpgradeCallback): Promise<PromisedDB>;
+}
